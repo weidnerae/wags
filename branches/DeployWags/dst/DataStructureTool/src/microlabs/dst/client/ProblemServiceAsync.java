8896016059a930e5ca39b2fb21e81c2940a4981b
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2011-09-15 16:21:04 +0000

    Lets see if this works

--- branches/DeployWags/dst/DataStructureTool/src/microlabs/dst/client/ProblemServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package microlabs.dst.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ProblemServiceAsync {
-	
-	void getProblem(int id, AsyncCallback<Problem> callback);
-}
--- branches/DeployWags/dst/DataStructureTool/src/microlabs/dst/shared/AvailableServices.java
+++
@@ -1,8 +0,0 @@
-package microlabs.dst.shared;
-
-public interface AvailableServices {
-	
-	public static final int GET_EMAIL_ADDR_AND_LOGOUT_URL = 0;
-	public static final int GET_REPORTS = 1;
-	public static final int PROBLEM_ID_OFFSET = 2;
-}
