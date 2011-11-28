c3a6a17daf1805e22f0d6992f328b34e61328819
mdusen9@gmail.com <mdusen9@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2011-11-28 03:20:36 +0000

    Just editing code indentation in this file, not WAGS interface.

--- branches/DeployWags/server/class/command/SaveFileContents.php
+++ branches/DeployWags/server/class/command/SaveFileContents.php
@@ -21,38 +21,38 @@ class SaveFileContents extends Command
         }
         
         $user = Auth::getCurrentUser();
-	$file = CodeFile::getCodeFileByName($_POST['name']);
-	$contents = $_POST['contents'];
-
-	/**
-	 * For some reason, the URL encoding on the client side does
-	 * not encode "+" correctly.  Thus, it is manually encoded
-	 * on the client side as %2B -> the correct HTML encoding.
-	 * Of course, now %2B is encoded and decoded, so this next
-	 * line manually replaces it with "+" again
-	 */
-	$contents = str_replace("%2B", "+", $contents);
-
-        if(!empty($file) && $file instanceof CodeFile){
-            // Update CodeFile.
-            $file->setContents($contents);
-            $file->setUpdated(time());
-            $file->save();
-        }else{
-            // Create new CodeFile.
-            $file = new CodeFile();
-            $file->setContents($contents);
-            $now = time();
-	    $file->setName($_REQUEST['name']);
-	    $file->setExerciseId(0);
-            $file->setOwnerId($user->getId());
-			$file->setSection($user->getSection());
-            $file->setUpdated($now);
-            $file->setAdded($now);
-            $file->save();
-        }
-
-        return JSON::success('File '.$_REQUEST['name'].' saved');
-    }
+		$file = CodeFile::getCodeFileByName($_POST['name']);
+		$contents = $_POST['contents'];
+	
+		/**
+		 * For some reason, the URL encoding on the client side does
+		 * not encode "+" correctly.  Thus, it is manually encoded
+		 * on the client side as %2B -> the correct HTML encoding.
+		 * Of course, now %2B is encoded and decoded, so this next
+		 * line manually replaces it with "+" again
+		 */
+		$contents = str_replace("%2B", "+", $contents);
+	
+			if(!empty($file) && $file instanceof CodeFile){
+				// Update CodeFile.
+				$file->setContents($contents);
+				$file->setUpdated(time());
+				$file->save();
+			}else{
+				// Create new CodeFile.
+				$file = new CodeFile();
+				$file->setContents($contents);
+				$now = time();
+				$file->setName($_REQUEST['name']);
+				$file->setExerciseId(0);
+				$file->setOwnerId($user->getId());
+				$file->setSection($user->getSection());
+				$file->setUpdated($now);
+				$file->setAdded($now);
+				$file->save();
+			}
+	
+			return JSON::success('File '.$_REQUEST['name'].' saved');
+		}
 }
 ?>
