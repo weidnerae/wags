c0911fc60d217588e425aa5244e697bb30a98317
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2012-01-26 13:51:22 +0000

    Forgot to remove these files from the repository

--- branches/DeployWags/client/src/webEditor/dst/client/AvailableServices.java
+++
@@ -1,8 +0,0 @@
-package webEditor.dst.client;
-
-public interface AvailableServices {
-	
-	public static final int GET_EMAIL_ADDR_AND_LOGOUT_URL = 0;
-	public static final int GET_REPORTS = 1;
-	public static final int PROBLEM_ID_OFFSET = 2;
-}
--- branches/DeployWags/client/src/webEditor/dst/client/EmailServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package webEditor.dst.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface EmailServiceAsync {
-	
-	void email(String problemName, AsyncCallback<String> callback);
-}
