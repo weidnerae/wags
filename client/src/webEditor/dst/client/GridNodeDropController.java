163db9df6638bb7bebcb4aba56ba6179353f1d6b
jonjonsonjr@gmail.com <jonjonsonjr@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2012-02-04 17:47:20 +0000

    Trying to allow Daniel to commit... something is broken.

---
+++ client/src/webEditor/dst/client/GridNodeDropController.java
@@ -0,0 +1,38 @@
+package webEditor.dst.client;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.allen_sauer.gwt.dnd.client.DragContext;
+import com.allen_sauer.gwt.dnd.client.drop.GridConstrainedDropController;
+public class GridNodeDropController extends GridConstrainedDropController {
+	private static GridNodeDropController dc;
+	private static EdgeCollection ec;
+	private static AbsolutePanel boundaryPanel;
+	private static int gridX;
+	private static int gridY;
+	
+	public static GridNodeDropController getInstance()
+	{
+		if(dc == null)
+			dc = new GridNodeDropController(boundaryPanel, gridX, gridY);
+		return dc;
+	}
+	
+	private GridNodeDropController(AbsolutePanel boundaryPanel, int gridX, int gridY)
+	{
+		super(boundaryPanel, gridX, gridY);
+	}
+		
+	public static void setFields(AbsolutePanel theBoundaryPanel,int theGridX, int theGridY)
+	{
+		boundaryPanel = theBoundaryPanel;
+		gridX=theGridX;
+		gridY=theGridY;
+		dc = new GridNodeDropController(boundaryPanel,gridX,gridY);
+	}
+ 
+	public void onDrop(DragContext context)
+	{
+		super.onDrop(context);
+		ec.updateEdgeDrawings();
+	}
+	
+}
