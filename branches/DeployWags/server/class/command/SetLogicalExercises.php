3cc90bd84f30bd76f81ba52f03b467cde2460688
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2012-02-06 04:08:26 +0000

    Added Radix Sort microlabs to the DST section listing, removed some ineffectual code in AddExercise

--- branches/DeployWags/server/class/command/SetLogicalExercises.php
+++ branches/DeployWags/server/class/command/SetLogicalExercises.php
@@ -17,22 +17,42 @@ class SetLogicalExercises extends Command
         
         if(isset($_POST['Traversals']) && $_POST['Traversals'] == 'on')
         {
-            $exercises = "BST Preorder Traversal (Help on)|BST Inorder Traversal (Help on)|BST Postorder Traversal (Help on)|BST Preorder Traversal (Help off)|BST Inorder Traversal (Help off)|BST Postorder Traversal (Help off)|";
+            $exercises = "BST Preorder Traversal (Help on)|".
+            "BST Inorder Traversal (Help on)|".
+            "BST Postorder Traversal (Help on)|".
+            "BST Preorder Traversal (Help off)|".
+            "BST Inorder Traversal (Help off)|".
+            "BST Postorder Traversal (Help off)|";
         }
 
         if(isset($_POST['InsertNodes']) && $_POST['InsertNodes'] == 'on')
         {
-            $exercises = $exercises."Insert Nodes into a BST 1|Insert Nodes into a BST 2|Insert Nodes into a BST 3|Insert Nodes into a BST 4|";
+            $exercises = $exercises."Insert Nodes into a BST 1|".
+            "Insert Nodes into a BST 2|Insert Nodes into a BST 3|".
+            "Insert Nodes into a BST 4|";
         }
 
         if(isset($_POST['BuildBST']) && $_POST['BuildBST'] == 'on')
         {
-            $exercises = $exercises."Binary Search Tree from Postorder Traversal 1|Binary Search Tree from Postorder Traversal 2|Binary Search Tree from Postorder Traversal 3|Binary Search Tree from Postorder Traversal 4|";
+            $exercises = $exercises.
+            "Binary Search Tree from Postorder Traversal 1|".
+            "Binary Search Tree from Postorder Traversal 2|".
+            "Binary Search Tree from Postorder Traversal 3|".
+            "Binary Search Tree from Postorder Traversal 4|";
         }
 
         if(isset($_POST['BuildBT']) && $_POST['BuildBT'] == 'on')
         {
-            $exercises = $exercises."Binary Tree from Pre/Inorder Traversals 1|Binary Tree from Pre/Inorder Traversals 2|Binary Tree from Pre/Inorder Traversals 3|Binary Tree from Pre/Inorder Traversals 4|";
+            $exercises = $exercises.
+            "Binary Tree from Pre/Inorder Traversals 1|".
+            "Binary Tree from Pre/Inorder Traversals 2|".
+            "Binary Tree from Pre/Inorder Traversals 3|".
+            "Binary Tree from Pre/Inorder Traversals 4|";
+        }
+
+        if(isset($_POST['RadixSort']) && $_POST['RadixSort'] == 'on')
+        {
+            $exercises = $exercises."RADIX Sort|";
         }
 
         $sectionNumber = Auth::getCurrentUser()->getSection();
