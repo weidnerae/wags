c3d1300de9bf18a990d5f3f018979235d5fa59cd
cookdr22@gmail.com <cookdr22@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2012-02-19 22:42:08 +0000

    Added rough versions of MaxHeap Insertion MaxHeap Deletion. Had to change the MaxHeap evaluation to ignore 97 as a parentless node so that the deletion test would work.

--- branches/DeployWags/client/src/webEditor/dst/client/EdgeCollection.java
+++ branches/DeployWags/client/src/webEditor/dst/client/EdgeCollection.java
@@ -145,25 +145,6 @@ public class EdgeCollection implements IsSerializable
 	
 	public void insertEdges(String[] edgePairs, ArrayList<Node> nodes)
 	{
-		/**
-		 * 	for(int i = 0; i < edgePairs.length; i++)
-		{
-			char parent = edgePairs[i].charAt(0);
-			char child  = edgePairs[i].charAt(1);
-			
-			Node n1 = null, n2 = null;
-			for(int j = 0; j < nodes.size(); j++)
-			{
-				if(nodes.get(j).getValue() == parent)
-					n1 = nodes.get(j);
-				else if(nodes.get(j).getValue() == child)
-					n2 = nodes.get(j);
-			}
-			EdgeUndirected eu = new EdgeUndirected(n1,n2, getInstance(), handler, removable);
-			eu.drawEdge();
-			edges.add(eu);
-		}
-		 */
 		for(int i = 0; i < edgePairs.length; i++)
 		{
 			String[] splitPairs= edgePairs[i].split(" ");
