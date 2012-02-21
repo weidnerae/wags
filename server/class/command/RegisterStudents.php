d79a69dde4621454960bafe3b69b1a1822782be7
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2012-02-21 21:17:22 +0000

    Working towards CSV registering, learning php.  Register students should still be retaining all originial functionality without being affected.

--- server/class/command/RegisterStudents.php
+++ server/class/command/RegisterStudents.php
@@ -13,63 +13,98 @@
 class RegisterStudents extends Command
 {
 	public function execute(){
+        $txtRegister = $csvRegister = false;
 
-		if(!isset($_POST['first_name']) || !isset($_POST['last_name'])){
-			return JSON::error("Error in posting names");
+        if(isset($_POST['first_name']) && isset($_POST['last_name']))
+            $txtRegister = true;
+
+        if(isset($_FILES['csvReg']))
+            $csvRegister = true;
+
+        if(!$txtRegister && !$csvRegister){
+            return JSON::warn("No one to register!");
+        }
+
+        // Handles adding users from the
+        // text box entries
+        if($txtRegister){
+		    $first_name = $_POST['first_name'];
+    		$last_name = $_POST['last_name'];
+            $numUsers = 0;
+            $err = "";
+
+            if(!$this->validateCounts($first_name, $last_name, $numUsers))
+                return JSON::error("Must have same number first and last names");
+
+            if(!$this->allNewUsers($first_name, $last_name, $numUsers, $err))
+    			return JSON::warn($err);
+
+            #Create new user profiles for every user.
+            #The users will not have an e-mail, and their password
+            #is currently set to password
+    		for($i = 0; $i < $numUsers; $i++){
+    			$user = new User();
+    			$user->setUsername("$last_name[$i].$first_name[$i]");
+    			$user->setEmail("unset");
+    			$user->setFirstName($first_name[$i]);
+    			$user->setLastName($last_name[$i]);
+    			$user->setPassword(md5("password"));
+    			$user->setSection(Auth::getCurrentUser()->getSection());
+    			$user->setLastLogin(time());
+    			$user->setAdded(time());
+	    		$user->setUpdated(time());
+		    	$user->setAdmin(0);
+
+	    		try{
+	    			$user->save();
+		    	}catch(Exception $e){
+		    		return JSON::error($e->getMessage());
+	    		}
+
+            }
 		}
 
-		$first_name = $_POST['first_name'];
-		$last_name = $_POST['last_name'];
-		$numFirst = 0;
-		$numLast = 0;
+        // Handles adding users from
+        // the csv file
+        if($csvRegister){
+            $csvReg = $_FILES['csvReg'];
 
-        #make sure the number of first names is the same
-        #as the number of last names
-        #For CSV, that the length % 2 = 0
-		for($i = 0; $i < count($first_name); $i++){
-			if($first_name[$i] != "") $numFirst++;
-			if($last_name[$i] != "") $numLast++;
-		}
-		if($numFirst != $numLast){
-			return JSON::error("Must have same number first and last names");	
-		}
-
-        #before processing any users, check to see if any of them are already
-        #registered.  This is done to keep half of the students from being
-        #registered and then creating this same error when the rest of the students
-        #go to be registered
-		for($i = 0; $i < $numFirst; $i++){
-			if(User::isusername("$last_name[$i].$first_name[$i]")){
-				return JSON::warn("$last_name[$i].$first_name[$i] is already registered");
-			}
-		}
-
-        #Create new user profiles for every user.
-        #The users will not have an e-mail, and their password
-        #is currently set to password
-        #TODO: Update first login to request e-mail
-		for($i = 0; $i < $numFirst; $i++){
-			$user = new User();
-			$user->setUsername("$last_name[$i].$first_name[$i]");
-			$user->setEmail("unset");
-			$user->setFirstName($first_name[$i]);
-			$user->setLastName($last_name[$i]);
-			$user->setPassword(md5("password"));
-			$user->setSection(Auth::getCurrentUser()->getSection());
-			$user->setLastLogin(time());
-			$user->setAdded(time());
-			$user->setUpdated(time());
-			$user->setAdmin(0);
-
-			try{
-				$user->save();
-			}catch(Exception $e){
-				return JSON::error($e->getMessage());
-			}
-		}
+        }
 
 		return JSON::success("Users added");
 		
 	}
 
+
+
+    #make sure the number of first names is the same
+    #as the number of last names
+    private function validateCounts($first_name, $last_name, &$numUsers){
+        $numFirst = $numLast = 0;
+
+		for($i = 0; $i < count($first_name); $i++){
+			if($first_name[$i] != "") $numFirst++;
+			if($last_name[$i] != "") $numLast++;
+		}
+		if($numFirst != $numLast){
+			return false; 
+		}
+        
+        $numUsers = $numFirst;
+        return true;
+    }   
+
+    #before processing any users, check to see if any of them are already
+    #registered.  This is done to keep half of the students from being
+    #registered and then creating this same error when the rest of the students
+    #go to be registered
+    private function allNewUsers($first_name, $last_name, $number, &$err){
+		for($i = 0; $i < $number; $i++){
+			if(User::isusername("$last_name[$i].$first_name[$i]")){
+                $err = "Failure: $last_name[$i].$first_name[$i] Already Exists";
+                return false;
+			}
+		}
+        return true;
+    }
 }
