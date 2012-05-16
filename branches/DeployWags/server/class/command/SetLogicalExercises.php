49b96ee665b78563e8e2a0ba64958739c51209fc
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2012-05-16 16:06:42 +0000

    Greater granularity for what logical microlabs are visible

--- branches/DeployWags/server/class/command/SetLogicalExercises.php
+++ branches/DeployWags/server/class/command/SetLogicalExercises.php
@@ -52,21 +52,70 @@ class SetLogicalExercises extends Command
             "Binary Tree from Pre/Inorder Traversals 4|";
         }
 
+        if(isset($_POST['MaxHeapInsert']) && $_POST['MaxHeapInsert'] == 'on')
+        {
+            $exercises = $exercises.
+            "MaxHeap Insertion 1|"."MaxHeap Insertion 2|".
+            "MaxHeap Insertion 3|"."MaxHeap Insertion 4|";
+        }
+
+        if(isset($_POST['MaxHeapDelete']) && $_POST['MaxHeapDelete'] == 'on')
+        {
+            $exercises = $exercises.
+            "MaxHeap Deletion 1|"."MaxHeap Deletion 2|".
+            "MaxHeap Deletion 3|"."MaxHeap Deletion 4|";
+        }
+
+        if(isset($_POST['MinHeapInsert']) && $_POST['MinHeapInsert'] == 'on')
+        {
+            $exercises = $exercises.
+			"MinHeap Insertion 1|"."MinHeap Insertion 2|".
+            "MinHeap Insertion 3|"."MinHeap Insertion 4|";
+        }
+
+        if(isset($_POST['MinHeapDelete']) && $_POST['MinHeapDelete'] == 'on')
+        {
+            $exercises = $exercises.
+            "MinHeap Deletion 1|"."MinHeap Deletion 2|".
+            "MinHeap Deletion 3|"."MinHeap Deletion 4|";
+        }
+
+        if(isset($_POST['MaxHeapBuild']) && $_POST['MaxHeapBuild'] == 'on')
+        {
+            $exercises = $exercises.
+            "MaxHeap 1|"."MaxHeap 2|"."MaxHeap 3|"."MaxHeap 4|";
+        }
+
+        if(isset($_POST['MinHeapBuild']) && $_POST['MinHeapBuild'] == 'on')
+        {
+            $exercises = $exercises.
+            "MinHeap 1|"."MinHeap 2|"."MinHeap 3|"."MinHeap 4|";
+        }
+        
+        if(isset($_POST['HeapSort']) && $_POST['HeapSort'] == 'on')
+        {
+            $exercises = $exercises.
+            "HeapSort 1|"."HeapSort 2|"."HeapSort 3|"."HeapSort 4|";
+        }
+
         if(isset($_POST['RadixSort']) && $_POST['RadixSort'] == 'on')
         {
-            $exercises = $exercises."RADIX Sort|".
-            "MaxHeap Insertion 1|"."MaxHeap Insertion 2|".
-            "MaxHeap Insertion 3|"."MaxHeap Insertion 4|".
-            "MaxHeap Deletion 1|"."MaxHeap Deletion 2|".
-            "MaxHeap Deletion 3|"."MaxHeap Deletion 4|".
-            "MaxHeap 1|"."MaxHeap 2|"."MaxHeap 3|"."MaxHeap 4|".
-			"MinHeap Insertion 1|"."MinHeap Insertion 2|".
-            "MinHeap Insertion 3|"."MinHeap Insertion 4|".
-            "MinHeap Deletion 1|"."MinHeap Deletion 2|".
-            "MinHeap Deletion 3|"."MinHeap Deletion 4|".
-            "MinHeap 1|"."MinHeap 2|"."MinHeap 3|"."MinHeap 4|".
-            "HeapSort Test with Duplicates|"."HeapSort Test without Duplicates|".
-            "Testing Heap with Duplicates|";
+            $exercises = $exercises."RADIX Sort|Radix Sort 2|";
+
+        }
+
+        if(isset($_POST['Kruskal']) && $_POST['Kruskal'] == 'on')
+        {
+            $exercises = $exercises."MST Kruskal's 1|".
+            "MST Kruskal's 2|MST Kruskal's 3|".
+            "MST Kruskal's 4|"; 
+        }
+
+        if(isset($_POST['Prim']) && $_POST['Prim'] == 'on')
+        {
+            $exercises = $exercises."MST Prim's 1|".
+            "MST Prim's 2|MST Prim's 3|".
+            "MST Prim's 4|"; 
         }
 
         $sectionNumber = Auth::getCurrentUser()->getSection();
