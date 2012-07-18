56aa6f4803ce1753c3625a72ccf4d2b43495c9cc
jonjonsonjr@gmail.com <jonjonsonjr@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2012-07-18 21:53:10 +0000

    Wrong file! Haha I am dumb.

--- branches/DeployWags/server/class/command/GetLogicalExercises.php
+++ branches/DeployWags/server/class/command/GetLogicalExercises.php
@@ -10,20 +10,31 @@ class GetLogicalExercises extends Command
         $exercises = str_replace("\n", "", $exercises);
         $exerciseArray = explode("|", $exercises);
 		sort($exerciseArray);
-        
         $submissions = DSTSubmission::getAllSubmissionsByUserID();
 		
 		
 		$result = array();
 		
+		// I haven't optimized this yet, but I don't think it's necessary 
+		// since the datasets are going to be pretty small
 		foreach ($exerciseArray as $exercise) {
+			$hasSubmission = false;
 			foreach ($submissions as $submission) {
 				if ($exercise == $submission['title']) {
-					$row = array('title' => $submission['title'],
+					$row = array('title' => $exercise,
 								 'success' => $submission['success']);
 					$result[] = $row;
+					$hasSubmission = true;
 					break;
 				}
+			}
+			
+			// Set success to 0 if the problem hasn't been submitted yet
+			// Also, ignore the empty first element in $exerciseArray
+			if (!$hasSubmission && $exercise != "") {
+				$row = array('title' => $exercise,
+							 'success' => 0);
+				$result[] = $row;
 			}
 		}
 
