a4902c01289fcd5676bfc9286605e724a51ebaad
jonjonsonjr@gmail.com <jonjonsonjr@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2012-08-01 18:52:50 +0000

    Added some comments.

--- server/class/command/GetDesc.php
+++ server/class/command/GetDesc.php
@@ -8,13 +8,24 @@ class getDesc extends Command
 		$exercise = Exercise::getExerciseByTitle($exTitle);
 		$description = $exercise->getDescription();
 		
+		/*
+		 * $description points to the soft link to the actual JPG, so this line
+		 * parses out the file name and prepends /tmp/ before it so we can get 
+		 * the actual path of the JPG
+		 */
 		$path = str_replace(WE_ROOT."/descriptions/", '/tmp/', $description);
 		
+		/*
+		 * If the file doesn't exist, we need to recreate it from the JPG stored
+		 * in the database
+		 */
 		if (!file_exists($path)) {
 			$image = $exercise->getDescriptionJPG();
 			file_put_contents($path, $image);
 		}
 		
+		// replace the location on disk with the specific URL,
+		// I think there is a better way to do this, but it works
 		$url = str_replace(WE_ROOT, WE_URL, $description);
 		
 		return JSON::success($url);
