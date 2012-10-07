12b27211dbbed0b0c712da44f2c0c541d2439b73
jonjonsonjr <jonjonsonjr@062e9602-64fa-fb65-0213-a643acedf34c>
2012-10-07 07:33:10 +0000

    Finished line numbers in Editor for the most part. It looks a lot better and (as far as I can tell) works as intended.
    Changed how the Results tab of Magnets displays output. Now everything in shown in the same TextArea, with the compiler results displaying first, following by the submitted code, formatted with line numbers.
    
    Also, fixed a small bug in Magnets where the directions were displaying escaped characters. I just fixed the cases of newlines and quotation marks, so I'm sure that more issues will pop up later. I doubt that doing this on the client side is the best way un-escape the characters.

--- branches/DeployWags/client/src/webEditor/magnet/client/ResultsPanelUi.java
+++ branches/DeployWags/client/src/webEditor/magnet/client/ResultsPanelUi.java
@@ -18,8 +18,9 @@ public class ResultsPanelUi extends Composite {
 	}
 
 	@UiField LayoutPanel layout;
-	@UiField AbsolutePanel testCode;
 	@UiField AbsolutePanel testResults;
+	static String code = "";
+	static String results = "";
 	static TextArea codeToTest;
 	static TextArea codeTestResults;
 	
@@ -38,21 +39,58 @@ public class ResultsPanelUi extends Composite {
 		codeToTest.setReadOnly(true);
 		codeToTest.setVisibleLines(25);
 		codeToTest.setSize("100%", "100%");
-		//add it to left side AbsolutePanel
-		testCode.add(codeToTest);
 		
 		codeTestResults = new TextArea();
 		codeTestResults.setReadOnly(true);
 		codeTestResults.setVisibleLines(25);
 		codeTestResults.setSize("100%", "100%");
+		
+		String test = codeTestResults.getText() + codeToTest.getText();
+		
+		codeTestResults.setText(test);
 		testResults.add(codeTestResults);
+		//testResults.add(codeToTest);
+		
 	}
 
 	public static void setResultsText(String s){
-		codeTestResults.setText(s);
+		results = s;
+		
+		codeTestResults.setText(results + "\n\n\n" + code);
 	}
 	
+	/**
+	 * This method takes a String of code, s, and formats 
+	 * it so that it has line numbers to the left, then 
+	 * stores that in the field code.
+	 * 
+	 * GWT can't do String.format, so I'm not sure how to 
+	 * properly make the line numbers right-aligned. Thus, 
+	 * I just have to if-statements to add spaces as necessary.
+	 * @param s
+	 */
 	public static void setCodeText(String s){
-		codeToTest.setText(s);
+		String[] linedCode = s.split("\n");
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < linedCode.length; i++) {
+			String spaces = "";
+			
+			if ((i + 1) / 100 == 0) {
+				spaces += " ";
+			}
+			
+			if ((i + 1) / 10 == 0) {
+				spaces += " ";
+			}
+			
+			sb.append(spaces);
+			sb.append(i + 1);
+			sb.append("  ");
+			sb.append(linedCode[i]);
+			sb.append("\n");
+		}
+		
+		code = sb.toString();
 	}
 }
