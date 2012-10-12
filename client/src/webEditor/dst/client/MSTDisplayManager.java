dd0d07bbdda6da81821981740e0367c7bf8bcd98
cookdr22 <cookdr22@062e9602-64fa-fb65-0213-a643acedf34c>
2012-10-12 02:36:24 +0000

    Graphs work in Chrome again. Had to add a good bit of magic because Chrome likes to be dumb.  Again it blows my mind that even IE handles some things better than Chrome does.

--- client/src/webEditor/dst/client/MSTDisplayManager.java
+++ client/src/webEditor/dst/client/MSTDisplayManager.java
@@ -43,6 +43,7 @@ public class MSTDisplayManager extends TreeDisplayManager implements
 		resetButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				removeWidgetsFromPanel();
+				removeWeightLabelsFromPanel();
 				addEdgeCancel();
 				resetRemoveEdgeButton();
 
