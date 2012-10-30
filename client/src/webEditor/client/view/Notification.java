672d38042ff434c3bda84ff3fa5d207727912244
pmeznar@gmail.com <pmeznar@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2012-10-30 20:55:30 +0000

    Ugh.
    So, I changed notifications to span the whole screen.  This was the only way I could get notifications when zoomed to at all look acceptable.  There doesn't seem to be any way to center them on the screen - please, please try, and hopefully you will succeed.
    
    So, for now, they span the whole screen, and only their Y position changes.
    
    I also changed the what the button looks like to change your password.
    
    A few other changes were made to fix some bugs.

--- client/src/webEditor/client/view/Notification.java
+++ client/src/webEditor/client/view/Notification.java
@@ -12,6 +12,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Window.ScrollEvent;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -21,103 +22,109 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class Notification extends View {
 
-	private static NotificationUiBinder uiBinder = GWT.create(NotificationUiBinder.class);
-	interface NotificationUiBinder extends UiBinder<Widget, Notification> {
-	}
-	
-	// Style for notification. Associated with notification.css
-	interface NotificationStyle extends CssResource {
-		String error();
-		String success();
-		String warning();
-	}
-	
-	// Timer is static so only one instance
-	// 	- Allows it to be cancelled if needed
-	private final static Timer t = new Timer() {
-		@Override
-		public void run()
-		{
-			Notification.clear();
-		}
-	};
+        private static NotificationUiBinder uiBinder = GWT.create(NotificationUiBinder.class);
+        private static PopupPanel popup;
+        interface NotificationUiBinder extends UiBinder<Widget, Notification> {
+        }
+        
+        // Style for notification. Associated with notification.css
+        interface NotificationStyle extends CssResource {
+                String error();
+                String success();
+                String warning();
+                String popuppanel();
+        }
+        
+        // Timer is static so only one instance
+        //      - Allows it to be cancelled if needed
+        private final static Timer t = new Timer() {
+                @Override
+                public void run()
+                {
+                        Notification.clear();
+                }
+        };
 
-	@UiField Label notificationArea;
-	@UiField PopupPanel panel;
-	@UiField NotificationStyle style;
+        @UiField Label notificationArea;
+        @UiField static PopupPanel panel;
+        @UiField static NotificationStyle style;
 
-	public Notification(int status, String msg) 
-	{
-		initWidget(uiBinder.createAndBindUi(this));
-		
-		// Panel should gain a little attention.
-		panel.setAnimationEnabled(true);
-		//panel.setAutoHideEnabled(true);
-		Element pEl = panel.getElement();
+        public Notification(int status, String msg) 
+        {
+                initWidget(uiBinder.createAndBindUi(this));
+                
+                // Panel should gain a little attention.
+                panel.setAnimationEnabled(true);
+                //panel.setAutoHideEnabled(true);
+                Element pEl = panel.getElement();
 
-		// Add class to panel so styling comes out right.
-		switch(status){
-			case WEStatus.STATUS_ERROR:
-				pEl.addClassName(style.error());
-				break;
-			case WEStatus.STATUS_SUCCESS:
-				pEl.addClassName(style.success());
-				break;
-			case WEStatus.STATUS_WARNING:
-				pEl.addClassName(style.warning());
-		}
+                // Add class to panel so styling comes out right.
+                switch(status){
+                        case WEStatus.STATUS_ERROR:
+                                pEl.addClassName(style.error());
+                                break;
+                        case WEStatus.STATUS_SUCCESS:
+                                pEl.addClassName(style.success());
+                                break;
+                        case WEStatus.STATUS_WARNING:
+                                pEl.addClassName(style.warning());
+                }
 
-		notificationArea.setText(msg);
-	}
+                notificationArea.setText(msg);
+        }
 
-	@Override
-	public WEAnchor getLink() {
-		return null;
-	}
-	
-	public static void notify(int status, String msg)
-	{
-		Notification.cancel();
-		Element na = DOM.getElementById("notification-area");
-		Notification.clear();
-		Notification n = new Notification(status, msg);
-		int cw = Window.getClientWidth(); // width of the window
-		int sl = Window.getScrollLeft();  // distance from left side of window to left side of viewport
-		
-		// Width of the notification is 40% of the width of the window (from CSS)
-		int nWidth = (int)(cw*.4);
-		
-		// Getting the viewport size, Super gross.
-		int oldLeft = Window.getScrollLeft();
-		int oldTop = Window.getScrollTop();
-		Window.scrollTo(Window.getClientWidth(), 0);
-		int viewportWidth = Window.getClientWidth()-Window.getScrollLeft();		
-		Window.scrollTo(oldLeft,oldTop);
-			
-		int xPos = (sl+(((int)viewportWidth/2)-(nWidth)/2));
-		
-		RootPanel.get().add(n,xPos,Window.getScrollTop());		
-		// Automatically clear notification in 5 seconds.
-		t.schedule(5000);
-	}
-	
-	public static void clear()
-	{
-		for(int i = 0; i < RootPanel.get().getWidgetCount(); i++){
-			if(RootPanel.get().getWidget(i) instanceof Notification)
-				RootPanel.get().remove(i);
-		}
-	}
-	
-	// Cancel timer's current schedule
-	public static void cancel()
-	{
-		t.cancel();
-	}
-	
-	@UiHandler("notificationArea")
-	void onPanelClick(ClickEvent event){
-		//Notification.clear();
-	}
-	
+        @Override
+        public WEAnchor getLink() {
+                return null;
+        }
+        
+        public static void notify(int status, String msg)
+        {
+                Notification.cancel();
+                Notification.clear();
+                Notification n = new Notification(status, msg);
+                
+                popup = new PopupPanel();
+                popup.getElement().addClassName(style.popuppanel());
+                popup.setWidth(Window.getClientWidth() - 8 +"px");
+                popup.add(n);
+                Notification.NotificationScroll nscroll = new Notification.NotificationScroll();
+                Window.addWindowScrollHandler(nscroll);
+                
+                RootPanel.get().add(popup,0,Window.getScrollTop());
+                // Automatically clear notification in 5 seconds.
+                t.schedule(3000);
+        }
+        
+        public static void clear()
+        {
+        	Element parent = DOM.getElementById("notification-area");
+        	for(int i = 0; i < parent.getChildCount(); i++){
+        		parent.removeChild(parent.getChild(i));
+        	}
+        	
+            for(int i = 0; i < RootPanel.get().getWidgetCount(); i++){
+                if(RootPanel.get().getWidget(i) instanceof PopupPanel){
+                	// In order to not also remove the password popup, we check to make sure this widget
+                	// is super long...
+                	if(RootPanel.get().getWidget(i).getOffsetWidth() >= Window.getClientWidth() - 8)
+                		RootPanel.get().remove(i);
+                }
+            }
+        }
+        
+        // Cancel timer's current schedule
+        public static void cancel()
+        {
+                t.cancel();
+        }
+        
+        private static class NotificationScroll implements Window.ScrollHandler{
+			@Override
+			public void onWindowScroll(ScrollEvent event) {
+				popup.setPopupPosition(0, Window.getScrollTop());				
+			}
+        	
+        }
+        
 }
