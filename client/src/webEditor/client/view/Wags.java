8ba770601b2f557fcfee870baf53edd8fcdf1cdf
jonjonsonjr <jonjonsonjr@062e9602-64fa-fb65-0213-a643acedf34c>
2012-11-07 09:06:18 +0000

    I made browser history work pretty well now.
    URL's will be formatted like:
    
    cs.appstate.edu/wags/#?loc=editor
    
    I think it would be better (i.e. make my code shorter) to do this:
    
    cs.appstate.edu/wags/#editor
    
    But I kind of think that looks ugly. If anyone wants to weigh in on how they feel about it, I'd love to hear your opinions. Part of me would prefer the second format, but I don't know how everyone else would feel about it.
    
    Also, I'd like to hear opinions on how we should handle the user clicking back to the login screen. Right now, I just have it log them out if they hit back and it goes to:
    
    cs.appstate.edu/wags/#?loc=login
    
    So that they are presented with the login page and have to log back in. I don't know how everyone feels about that, though. How should we handle it?

--- client/src/webEditor/client/view/Wags.java
+++ client/src/webEditor/client/view/Wags.java
@@ -5,10 +5,9 @@ import webEditor.client.MagnetProblem;
 import webEditor.client.Proxy;
 import webEditor.client.WEStatus;
 import webEditor.magnet.client.RefrigeratorMagnet;
-import webEditor.magnet.client.SplashPage;
+import webEditor.magnet.client.Magnets;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.Overflow;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
@@ -17,7 +16,6 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -44,7 +42,7 @@ public class Wags extends View
 	@UiField Anchor logout;
 	@UiField Label hello;
 
-	private SplashPage splashPage;
+	public Magnets splashPage;
 	private Editor editor;
 	
 	private String startingPlace;
@@ -60,83 +58,85 @@ public class Wags extends View
 		Proxy.getUsersName(hello, Editor, DST, Magnets, startingPlace);
 		Proxy.checkPassword(this);
 		Proxy.checkMultiUser(this);
+		Proxy.setWags(this);
 		
 		this.startingPlace = startingPlace;
-		splashPage = new SplashPage(this);
-		splashPage.getElement().getStyle().setOverflowY(Overflow.AUTO);
 		editor = new Editor();
 		
 		// Load the correct initial page
 		if (startingPlace.equals("magnets")) {
-			Magnets.setVisible(true);
-			replaceCenterContent(splashPage);
-			History.newItem("?loc=magnets");
+			loadMagnets();
 		} else if (startingPlace.equals("dst")) {
-			Proxy.buildDST(this);
-			History.newItem("?loc=dst");
+			loadDST();
 		} else {
-			replaceCenterContent(editor);
-			History.newItem("?loc=editor");
+			loadEditor();
 		}
 		
 		//Make back/forward buttons work.
+		createHistoryHandler();
+	}
+	
+	/**
+	 * This creates the ValueChangeHandler that makes clicking back/forward 
+	 * in the browser work correctly.
+	 */
+	private void createHistoryHandler() {
 		History.addValueChangeHandler(new ValueChangeHandler<String>() {
 
 			@Override
 			public void onValueChange(ValueChangeEvent<String> event) {
 				String url = event.getValue();
 				if (url.endsWith("editor")) {
-					Editor.fireEvent(new ClickEvent() { });
+					loadEditor();
 				} else if (url.endsWith("dst")) {
-					DST.fireEvent(new ClickEvent() { });
+					loadDST();
 				} else if (url.endsWith("magnets")) {
-					Magnets.fireEvent(new ClickEvent() { });
+					loadMagnets();
+				} else if (url.endsWith("login")) {
+					Proxy.logout();
 				}
 			}
 			
 		});
-	} // end constructor
+	}
 
 	@UiHandler("Editor")
 	void onEditorClick(ClickEvent event)
 	{
-		replaceCenterContent(editor);
-		
-		if (!Window.Location.getHref().endsWith("editor")) {
-			History.newItem("?loc=editor");
-		}
+		loadEditor();
 	}
+	
 	@UiHandler("DST")
 	void onDSTClick(ClickEvent event)
 	{
-		Proxy.buildDST(this);
-		
-		if (!Window.Location.getHref().endsWith("dst")) {
-			History.newItem("?loc=dst");
-		}
+		loadDST();
 	}
+	
 	@UiHandler("Magnets")
 	void onMagnetsClick(ClickEvent event)
 	{
-		// Have to construct a new splashpage in case the administrator
-		// has changed available exercises
-		splashPage = new SplashPage(this);
-		splashPage.getElement().getStyle().setOverflowY(Overflow.AUTO);
-		replaceCenterContent(splashPage);
-		splashPage.getWidget(0).setVisible(true);
-		
-		if (!Window.Location.getHref().endsWith("magnets")) {
-			History.newItem("?loc=magnets");
-		}
+		loadMagnets();
 	}
 
-	/**
-	 * Logout!	
-	 */
 	@UiHandler("logout")
 	void onLogoutClick(ClickEvent event)
 	{
 		Proxy.logout();
+	}
+	
+	public void loadEditor() {
+		replaceCenterContent(editor);
+		History.newItem("?loc=editor");
+	}
+	
+	public void loadDST() {
+		Proxy.buildDST(this);
+		History.newItem("?loc=dst");
+	}
+	
+	public void loadMagnets() {
+		Proxy.buildMagnets(this);
+		History.newItem("?loc=magnets");
 	}
 	
 
@@ -221,9 +221,10 @@ public class Wags extends View
 		for(int i=0;i<dock.getWidgetCount();i++){
 			if(dock.getWidgetDirection(dock.getWidget(i))==DockLayoutPanel.Direction.CENTER){
 				dock.remove(i);
-				dock.add(w);
 			}
 		}
+		
+		dock.add(w);
 	}
 	
 	public void replaceCenterContent(Widget w){
@@ -231,14 +232,9 @@ public class Wags extends View
 			if(dock.getWidgetDirection(dock.getWidget(i))==DockLayoutPanel.Direction.CENTER){
 					dock.remove(i);
 			}
-			dock.add(w);
 		}
-	}
-	
-	public void updateSplashPage(VerticalPanel problemPane){
 		
-		splashPage.remove(0);  // removes old problemPane ?
-		splashPage.add(problemPane);
+		dock.add(w);
 	}
 	
 	@Override
