3ebb4c545ea4063a2b8db3241afd8f75fdb6e73e
side.weidner@gmail.com <side.weidner@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-03-20 05:33:55 +0000

    Potentially fixed magnet maker interaction with addSegments function in ConstructUI.  Using an array to keep track of the previous magnets calculated baseY value and the method checks when that value dips below the last value, meaning that the panel has been scrolled upward and the methods getTop are now returning bad (for our purposes) values.  Tried it in all the use cases I could think of and it worked as intended with no adverse effects.  Should still be tested for edge cases however.  Also button tweak for logical and layout tweak on codepanel to make it align with construct panel more.

--- branches/DeployWags/client/src/wags/magnet/view/ConstructUi.java
+++ branches/DeployWags/client/src/wags/magnet/view/ConstructUi.java
@@ -13,6 +13,7 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.Window;
 
 /**
  * 
@@ -28,7 +29,8 @@ public class ConstructUi extends Composite {
 	private AbsolutePanel mmContent;    //nest panel to hold magnet maker
 	private CreationStation magnetMaker;
 	private AbsolutePanel segmentsContent;
-	private AbsolutePositionDropController segmentDropControl; 
+	private AbsolutePositionDropController segmentDropControl;
+	private int[] segmentTops = new int[50];  //used to keep track of coordinates for added segments
 	private int nextID; // used for assigning created magnets ID's
 	
 	@UiField
@@ -184,28 +186,53 @@ public class ConstructUi extends Composite {
             	//Spacing is 10 pixels
                 int baseX = 10;
                 int baseY = 10;
-                if(problemType.equals(Consts.ADVANCED_PROBLEM)) 
-                	baseY = 10 + magnetMaker.getOffsetHeight();
-      
-                                            
+                                              
                 if (widgetCount == 0) {
-                        // we add the first widget at an offset of 10, 10"
+                        // we add the first widget at an offset of 10, 10
                         segmentsContent.add(segment, baseX, baseY);
                 } else {
                         // after the first, we calculate where the next widget should go based on the last widget in the panel
                         StackableContainer lastWidget = (StackableContainer) segmentsContent.getWidget(segmentsContent.getWidgetCount() - 1);
-                             
-                        baseY = lastWidget.getTop() + lastWidget.getHeight() - getAbsoluteTop();
                         
-  //    }   ADD THIS BACK.
-//                                  // for the first set of magnets (i.e. before the user adds any they created), we have to have a special
-                        // flag and treat them differently
-                        if (initial) {
-                                baseY -= segmentsContent.getAbsoluteTop() - getAbsoluteTop();
-                        } else if (problemType.equals(Consts.ADVANCED_PROBLEM)) { 
-                   //             baseY -= magnetMaker.getOffsetHeight();
+                        //Window.alert("lastWidget.getTop(): " + lastWidget.getTop() + "\nlast.Widget.getHeight():  " + lastWidget.getHeight() + "\ngetAbsoluteTop(): " + getAbsoluteTop() + "\ncalculated baseY: " + ((lastWidget.getTop() + lastWidget.getHeight() - getAbsoluteTop()) - (segmentsContent.getAbsoluteTop() - getAbsoluteTop())));
+                        
+                        //get raw position information for last widget placed in segmentscontent
+                        //subtract it from absolute top of constructUI to account for tabs and anchor
+                        baseY = lastWidget.getTop() + lastWidget.getHeight() - getAbsoluteTop();
+                        //subtract the length from the top of segments content up to the top of the UI
+                        baseY -= segmentsContent.getAbsoluteTop() - getAbsoluteTop();
+                        
+                        //store the calculated baseY in case it is needed after scrolling
+                        segmentTops[widgetCount - 1] = baseY;
+                        
+                        //check the current baseY against the relative top (baseY) 
+                        //of the widget before the last one.  if the current one is smaller
+                        //replace it with the one before hand and calculate the correct baseY
+                        //by adding the height of the last widget added + 10 to the old baseY
+                        if (widgetCount != 1 && baseY < segmentTops[widgetCount - 2]) {
+                        	baseY = segmentTops[widgetCount - 2];
+                        	baseY += lastWidget.getHeight() + 10;
+                        	segmentTops[widgetCount -1] = baseY;
                         }
-                } // get rid of this.
+                        
+                        /**
+                         * I removed this because I noticed no change between this and simply removing
+                         * length of everything above segmentsContent for every piece, regardless of if
+                         * it was the initial pieces or the created pieces.  If whoever wrote the code
+                         * (Daniel I think) has a reason for an edge case or something that I didn't find
+                         * I'll uncomment, otherwise I'll delete this - Alex 3/20/13
+                        // for the first set of magnets (i.e. before the user adds any they created), we have to have a special
+                        // flag and treat them differently
+                        //if (initial) {
+                        //        baseY -= segmentsContent.getAbsoluteTop() - getAbsoluteTop();
+                        //		Window.alert("initial flag: true\nbaseY: " + baseY);
+                        //} else if (problemType.equals(Consts.ADVANCED_PROBLEM)) { 
+                        //        baseY -= magnetMaker.getOffsetHeight();
+                        //        Window.alert("initial flag: false\nbaeY: " + baseY);
+                        //}
+                         * 
+                         */
+                }
                         segmentsContent.add(segment, baseX, baseY);
             }  
         };
