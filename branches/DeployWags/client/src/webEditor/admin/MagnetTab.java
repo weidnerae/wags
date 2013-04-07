edbb2853dca0cb6c0bfe7884efc180f4200e50b3
side.weidner@gmail.com <side.weidner@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-04-07 09:02:50 +0000

    Beginning work on the magnets page.  Modeled two Proxy functions after the ones Philip made for logical microlabs to populate the button panels correctly.  We have a problem in that the magnet list is too long and causes an obnoxious amount of scrolling and there is an inordinate amount of white space inbetween the 3 modules because they are particularly narrow.  I think it would be best to fix this with an extended and modified ButtonPanel that for every 10th button or so adds them to the next column of buttons, but I'll take care of that in a future commit.

---
+++ branches/DeployWags/client/src/webEditor/admin/MagnetTab.java
@@ -0,0 +1,82 @@
+package webEditor.admin;
+
+import webEditor.Proxy;
+import webEditor.ProxyFacilitator;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class MagnetTab extends Composite implements ProxyFacilitator {
+
+	private static MagnetTabUiBinder uiBinder = GWT
+			.create(MagnetTabUiBinder.class);
+
+	interface MagnetTabUiBinder extends UiBinder<Widget, MagnetTab> {
+	}
+	
+	@UiField ButtonPanel btnPanelGroups;
+	@UiField CheckBoxPanel chkPanelExercises;
+	@UiField AssignedPanel asPanel;
+
+	public MagnetTab() {
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		Proxy.getMMGroups(this);
+		Proxy.getMMExercises("Default", this);
+		
+		btnPanelGroups.setTitle("GROUPS");
+		chkPanelExercises.setTitle("EXERCISES");
+		chkPanelExercises.setAssignedPanel(asPanel);
+
+		addGroupClickHandlers();
+	}
+
+	@Override
+	public void handleSubjects(String[] subjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleGroups(String[] groups) {
+		btnPanelGroups.addButtons(groups);
+		addGroupClickHandlers();
+	}
+
+	@Override
+	public void handleExercises(String[] exercises) {
+		chkPanelExercises.addCheckBoxes(exercises);
+	}
+
+	//-------------------------------
+	// Group panel click handling
+	//-------------------------------
+	private void addGroupClickHandlers(){
+		for(int i = 0; i < btnPanelGroups.myButtons.size(); i++){
+			Button tmpBtn = btnPanelGroups.myButtons.get(i);
+			tmpBtn.addClickHandler(new groupClickHandler(tmpBtn.getText(), this));
+		}
+	}
+	
+	private class groupClickHandler implements ClickHandler{
+		String title;
+		ProxyFacilitator pf;
+
+		public groupClickHandler(String title, ProxyFacilitator pf){
+			this.title = title;
+			this.pf = pf;
+		}
+		@Override
+		public void onClick(ClickEvent event) {
+			Proxy.getMMExercises(title, pf);
+		}
+		
+	}
+	
+}
