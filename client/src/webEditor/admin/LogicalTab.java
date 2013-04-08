e3420c071b770baece9d352479db5387be9877fb
pmeznar@gmail.com <pmeznar@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-04-08 02:42:23 +0000

    Logical Section is up and running, without review capabilities (for now).
    Added an "Assigned" AssignedPanel (ha), which shows what exercises are currently assigned.  Checkboxes are automatically filled with what is already assigned.
    
    Re-did some of the layout.  Not sure how it'll work on a small screen, but we'll get that set up with some overflows.
    
    Added more methods to ProxyFacilitator.  getCallback and setCallback should end up being quite helpful in cleaning up Proxy once we are able to remove all the old admin stuff.
    
    Anyway, y'all should take a look.  It's much nicer than what we had before, and much more object-oriented as well.  Proxy is going to get better!
    
    Oh.  However, there are some changes we have to make on the server to fix Proxy.  Basically, this doubling up getting what is assigned/what is completed/what should be reviewed in one array is not good.  It's my fault, but it is not good.

--- client/src/webEditor/admin/LogicalTab.java
+++ client/src/webEditor/admin/LogicalTab.java
@@ -1,7 +1,10 @@
 package webEditor.admin;
 
+import java.util.HashMap;
+
 import webEditor.Proxy;
 import webEditor.ProxyFacilitator;
+import webEditor.WEStatus;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -9,6 +12,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -23,7 +27,7 @@ public class LogicalTab extends Composite implements ProxyFacilitator{
 	@UiField ButtonPanel btnPanelSubjects;
 	@UiField ButtonPanel btnPanelGroups;
 	@UiField CheckBoxPanel chkPanelExercises;
-	@UiField AssignedPanel asPanel;
+	@UiField AssignedPanel asPanel, asAlreadyPanel;
 
 	public LogicalTab() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -32,17 +36,21 @@ public class LogicalTab extends Composite implements ProxyFacilitator{
 		Proxy.getLMSubjects(this);
 		Proxy.getLMGroups("Binary Trees", this);
 		Proxy.getLMExercises("Traversals", this);
+		Proxy.getLMAssigned(this);
 		
 		// Initial set up
 		btnPanelSubjects.setTitle("SUBJECTS");
-		
 		btnPanelGroups.setTitle("GROUPS");
-		
 		chkPanelExercises.setTitle("EXERCISES");
 		chkPanelExercises.setAssignedPanel(asPanel);
 		
+		asPanel.setTitle("SELECTED");
+		asAlreadyPanel.setTitle("ASSIGNED");
+		asAlreadyPanel.btnAssign.setVisible(false);
+		
 		addSubjectClickHandlers();
 		addGroupClickHandlers();
+		asPanel.setParent(this);
 	}
 	
 	//-------------------------------
@@ -95,6 +103,9 @@ public class LogicalTab extends Composite implements ProxyFacilitator{
 		
 	}
 
+	//-----------------------------
+	// Proxy facilitation
+	//-----------------------------
 	@Override
 	public void handleSubjects(String[] subjects) {
 		btnPanelSubjects.addButtons(subjects);
@@ -110,6 +121,43 @@ public class LogicalTab extends Composite implements ProxyFacilitator{
 	@Override
 	public void handleExercises(String[] exercises) {
 		chkPanelExercises.addCheckBoxes(exercises);
+	}
+	
+	public void setExercises(String[] exercises){
+		String toAssign = "";
+		for(int i = 0; i < exercises.length; i++){
+			toAssign += exercises[i] + "|";
+		}
+		Proxy.SetLMExercises(toAssign, this);
+	}
+
+	public void setCallback(String[] exercises, int status) {
+		if(status == WEStatus.STATUS_SUCCESS){
+			asAlreadyPanel.clear();
+			
+			for(int i = 0; i < exercises.length; i++){
+				asAlreadyPanel.add(exercises[i]);
+			}
+		}
+	}
+
+	@Override
+	public void getCallback(String[] exercises, int status) {
+		HashMap<String, CheckBox> chkBoxes = chkPanelExercises.getAssignments();
+		for(int i = 0; i < exercises.length; i++){
+			asAlreadyPanel.add(exercises[i]);
+			asPanel.add(exercises[i]);
+			
+			// Handles checking assigned exercises
+			if(chkBoxes.containsKey(exercises[i])){
+				chkBoxes.get(exercises[i]).setValue(true);
+			} else {
+				CheckBox tmpCheck = new CheckBox(exercises[i]);
+				chkPanelExercises.addClickHandler(tmpCheck);
+				tmpCheck.setValue(true);
+				chkBoxes.put(exercises[i], tmpCheck);
+			}
+		}
 	}
 
 }
