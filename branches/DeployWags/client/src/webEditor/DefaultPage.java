9b9c70fd56b96bd23fb8a74f0088a2332cb47295
pmeznar@gmail.com <pmeznar@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-04-11 20:05:47 +0000

    Removed the old admin page and a few proxy methods.  I'm sure there's many more proxy methods we can remove now, but only the ones that had a reference to Admin got flagged by eclipse.

--- branches/DeployWags/client/src/webEditor/DefaultPage.java
+++ branches/DeployWags/client/src/webEditor/DefaultPage.java
@@ -1,16 +1,12 @@
 package webEditor;
 
-import webEditor.admin.AdminPage;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 
 public class DefaultPage extends Composite {
---
+++ branches/DeployWags/client/src/webEditor/admin/ProgrammingTab.java
@@ -0,0 +1,147 @@
+package webEditor.admin;
+
+import webEditor.Notification;
+import webEditor.Proxy;
+import webEditor.WEStatus;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.SubmitButton;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteHandler;
+
+public class ProgrammingTab extends Composite {
+
+	private static ProgrammingTabUiBinder uiBinder = GWT
+			.create(ProgrammingTabUiBinder.class);
+
+	interface ProgrammingTabUiBinder extends UiBinder<Widget, ProgrammingTab> {
+	}
+	
+	@UiField SubmitButton addButton;
+	@UiField ListBox exercises;
+	@UiField Button btnAdminReview, btnAddSkeletons, btnMakeVisible;
+	@UiField Grid grdAdminReview;
+	@UiField FileUpload testClass, helperClass, solution, skeleton;
+	@UiField FormPanel helperForm, adminForm;
+	@UiField TextBox openDate, closeDate, fileName;
+
+	public ProgrammingTab() {
+		initWidget(uiBinder.createAndBindUi(this));
+		Proxy.getVisibleExercises(exercises); 
+		
+		//Handle the Add Exercise Form
+		adminForm.setAction(Proxy.getBaseURL() + "?cmd=AddExercise");
+		adminForm.setEncoding(FormPanel.ENCODING_MULTIPART);
+		adminForm.setMethod(FormPanel.METHOD_POST);
+			
+		adminForm.addSubmitCompleteHandler(new SubmitCompleteHandler(){
+			
+			@Override
+			public void onSubmitComplete(SubmitCompleteEvent event) {
+				WEStatus stat = new WEStatus(event.getResults());
+				
+				Notification.notify(stat.getStat(), stat.getMessage());
+				Proxy.getVisibleExercises(exercises); 
+				
+				if(stat.getStat() == WEStatus.STATUS_SUCCESS){
+					// Message is of the form: 'Uploaded Exercise [exercise title]'
+					// So, exercise, exercise title begins at index 18
+					String exName = stat.getMessage().substring(18);
+					Proxy.addSkeletons(exName);
+				}
+				
+			}
+		});
+		
+		//Handle the Actions on Exercises Form
+		helperForm.setAction(Proxy.getBaseURL() + "?cmd=AddHelperClass");
+		helperForm.setEncoding(FormPanel.ENCODING_MULTIPART);
+		helperForm.setMethod(FormPanel.METHOD_POST);
+		
+		helperForm.addSubmitCompleteHandler(new SubmitCompleteHandler(){
+			
+			//didn't want to create a whole proxy call, so added this instead
+			public void onSubmitComplete(SubmitCompleteEvent event) {
+				int status = WEStatus.STATUS_SUCCESS;
+				if(event.getResults() != "Upload Successful") status = WEStatus.STATUS_ERROR; 
+				Notification.notify(status, event.getResults());
+			}
+		});
+	}
+	
+	@UiHandler("btnAdminReview")
+	void onReviewClick(ClickEvent event)
+	{
+		grdAdminReview.clear(true);
+		Proxy.getSubmissionInfo(exercises.getValue(exercises.getSelectedIndex()), grdAdminReview);		
+	}
+	
+	@UiHandler("btnAddSkeletons")
+	void onSkelClick(ClickEvent event){
+		Proxy.alterExercise(exercises.getValue(exercises.getSelectedIndex()), "skel", exercises);
+	}
+	
+	@UiHandler("btnMakeVisible")
+	void onVisClick(ClickEvent event){
+		Proxy.alterExercise(exercises.getValue(exercises.getSelectedIndex()), "vis", exercises);
+	}
+	
+	@UiHandler("btnDeleteExercise")
+	void deleteExerciseClick(ClickEvent event){
+		final DialogBox deleteExercise = new DialogBox(false);
+		Label lbl1 = new Label("ARE YOU SURE?  This deletes ALL associated\n" +
+								"files and submissions, and IS NOT RECOVERABLE.");
+		
+		Button delete = new Button("DELETE");
+		Button nvm = new Button("Nevermind");
+		
+		VerticalPanel base = new VerticalPanel();
+		HorizontalPanel line1 = new HorizontalPanel();
+		HorizontalPanel line2 = new HorizontalPanel();
+				
+		deleteExercise.setText("DELETING " + exercises.getValue(exercises.getSelectedIndex()).toUpperCase());
+				
+		delete.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				deleteExercise.hide();
+				Proxy.deleteExercise(exercises.getValue(exercises.getSelectedIndex()), exercises);
+			}
+		});
+		
+		nvm.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				deleteExercise.hide();				
+			}
+		});
+		
+		line1.add(lbl1);
+		line2.add(nvm);
+		line2.add(delete);
+		base.add(line1);
+		base.add(line2);
+		deleteExercise.add(base);
+		deleteExercise.center();
+	}
+
+}
