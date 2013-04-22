6beacd0b28a011cce917c56bb4bcc794f43f9971
pmeznar@gmail.com <pmeznar@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-04-22 02:35:06 +0000

    A little more flexibility for playing around and testing if you want

--- client/src/webEditor/admin/builders/LMTraversalDisplay.java
+++ client/src/webEditor/admin/builders/LMTraversalDisplay.java
@@ -4,8 +4,12 @@ package webEditor.admin.builders;
 import webEditor.admin.LMDisplay;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -20,13 +24,28 @@ public class LMTraversalDisplay extends LMDisplay {
 
 	LMBuilder builder;
 	@UiField VerticalPanel basePanel;
+	@UiField BasicBuilder canvas;
+	@UiField Button btnAddNode;
+	@UiField TextBox txtAddNode;
 	
 	public LMTraversalDisplay() {
 		initWidget(uiBinder.createAndBindUi(this));
+		
+		btnAddNode.addClickHandler(new ClickHandler() {
+			
+			public void onClick(ClickEvent event) {
+				String val = txtAddNode.getText();
+				if(val.length() > 0){
+					canvas.addNode(txtAddNode.getText());
+					txtAddNode.setText("");
+				}
+			}
+		});
 	}
 	
 	public void setBuilder(LMBuilder builder){
 		this.builder = builder;
 	}
+	
 
 }
