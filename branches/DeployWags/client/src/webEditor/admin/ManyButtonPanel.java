2b862c762a8d41b1c6464764236f5fb32e47b405
side.weidner@gmail.com <side.weidner@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-04-30 01:00:46 +0000

    Fixing text on buttons issue in firefox.  Took away bold because in firefox it was SUPER bold, and then bumped up button height by 10 px just in case we come up with even longer exercise names in the past.  Also made it so that review panels used many button panels so that you didnt scroll through a straight line of 40 something magnet problems.

--- branches/DeployWags/client/src/webEditor/admin/ManyButtonPanel.java
+++ branches/DeployWags/client/src/webEditor/admin/ManyButtonPanel.java
@@ -27,6 +27,7 @@ public class ManyButtonPanel extends Composite
 	@UiField VerticalPanel btnHolder;
 	@UiField VerticalPanel btnHolder2;
 	@UiField VerticalPanel btnHolder3;
+	@UiField VerticalPanel btnHolder4;
 	@UiField HorizontalPanel btnHolderHolder;
 	ArrayList<Button> myButtons = new ArrayList<Button>();
 	private int btnWidth = 175;
@@ -39,6 +40,7 @@ public class ManyButtonPanel extends Composite
 		btnHolder.setSpacing(CELL_SPACING);
 		btnHolder2.setSpacing(CELL_SPACING);
 		btnHolder3.setSpacing(CELL_SPACING);
+		btnHolder4.setSpacing(CELL_SPACING);
 	}
 	
 	public void setTitle(String title){
@@ -49,6 +51,7 @@ public class ManyButtonPanel extends Composite
 		btnHolder.clear();
 		btnHolder2.clear();
 		btnHolder3.clear();
+		btnHolder4.clear();
 		myButtons.clear();
 		Button tmpBtn;
 		
@@ -64,12 +67,14 @@ public class ManyButtonPanel extends Composite
 	private void addButton( Button tmpBtn )
 	{
 		int numberOfButtons = myButtons.size();
-		if (numberOfButtons <= 10) {
+		if (numberOfButtons <= 15) {
 			this.btnHolder.add(tmpBtn);
-		} else if (numberOfButtons <= 20) {
-			this.btnHolder2.add(tmpBtn);
 		} else if (numberOfButtons <= 30) {
+			this.btnHolder2.add(tmpBtn);
+		} else if (numberOfButtons <= 45) {
 			this.btnHolder3.add(tmpBtn);
+		} else if (numberOfButtons <= 60) {
+			this.btnHolder4.add(tmpBtn);
 		}
 	}
 	
@@ -103,7 +108,7 @@ public class ManyButtonPanel extends Composite
 		
 	}
 	
-	private void colorBlack(){
+	void colorBlack(){
 		for(Button btn: myButtons){
 			btn.getElement().getStyle().setColor("black");
 		}
@@ -114,6 +119,8 @@ public class ManyButtonPanel extends Composite
 			btn.addClickHandler(new btnClickHandler(btn));
 		}
 	}
+	
+	
 
 
 }
