9af30081974f689a456662d40fcf4d50ff0b1c75
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2013-06-05 20:44:50 +0000

    Can now create/upload binary tree traversal exercises just fine..

--- branches/DeployWags/server/class/command/UploadLogicalMicrolab.php
+++ branches/DeployWags/server/class/command/UploadLogicalMicrolab.php
@@ -1,6 +1,9 @@
 <?php
 
-class LoadLogicalMicrolab extends Command{
+class UploadLogicalMicrolab extends Command{
+    // Will have to add a fair amount of flexibility to this guy to get
+    // all sorts of different logical microlabs uploading...
+    // Really, just how to get it to store as null when a $_GET is 'empty'
     public function execute(){
         $checkLm = LogicalMicrolab::getLogicalMicrolabByTitle($_GET['title']);
         if(!empty($checkLm)){
@@ -11,14 +14,14 @@ class LoadLogicalMicrolab extends Command{
         $lm->setTitle($_GET['title']);
         $lm->setProblemText($_GET['problemText']);
         $lm->setNodes($_GET['nodes']);
-        //$lm->setXPositions($_GET['xPositions']);
-        //$lm->setYPositions($_GET['yPositions']);
+        $lm->setXPositions($_GET['xPositions']);
+        $lm->setYPositions($_GET['yPositions']);
         $lm->setInsertMethod($_GET['insertMethod']);
-        //$lm->setEdges($_GET['edges']);
+        $lm->setEdges($_GET['edges']);
         $lm->setEvaluation($_GET['evaluation']);
         $lm->setEdgeRules($_GET['edgeRules']);
         $lm->setArguments($_GET['arguments']);
-        //$lm->setEdgesRemovable($_GET['edgesRemovable']);
+        $lm->setEdgesRemovable($_GET['edgesRemovable']);
         $lm->setNodesDraggable($_GET['nodesDraggable']);
         $lm->setNodeType($_GET['nodeType']);
         $lm->setGenre($_GET['genre']);
@@ -26,6 +29,10 @@ class LoadLogicalMicrolab extends Command{
         $lm->setAdded(time());
 
         $lm->save();
+
+
+        LogicalMicrolab::addToLMExercise($_GET['title'], $_GET['group']);
+
         $title = $_GET['title'];
         return JSON::success("Saved $title");
     }
