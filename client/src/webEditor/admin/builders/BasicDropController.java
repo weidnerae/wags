e4c69c527626888f0661ec5be0c9b503ec287213
side.weidner@gmail.com <side.weidner@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-07-16 03:47:01 +0000

    A possible solution to the nondeterministic order of re-adding weights we had problems with in Kruskal Problem creation.  Now when you move weighted nodes they are simply redrawn as-is.  You can edit any existing weight by single clicking on the label.  Modification to edges for graphs so that they require a double click to delete, in an attempt to reduce user error between changing label and removing edge.  Looking for feedback if any of you feel this isn't an optimal solution, am open to change it myself.

--- client/src/webEditor/admin/builders/BasicDropController.java
+++ client/src/webEditor/admin/builders/BasicDropController.java
@@ -2,7 +2,6 @@ package webEditor.admin.builders;
 
 import com.allen_sauer.gwt.dnd.client.DragContext;
 import com.allen_sauer.gwt.dnd.client.drop.AbsolutePositionDropController;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 
 public class BasicDropController extends AbsolutePositionDropController {
