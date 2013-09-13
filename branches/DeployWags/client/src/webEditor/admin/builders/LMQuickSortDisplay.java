95ae1cc711bb6e4907da220c8aebc6b38eff2391
cookdr22@gmail.com <cookdr22@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-09-13 01:56:46 +0000

    Making the fake workshop admins have the admin navigation link. Allowing 2 digit negative numbers in Quicksort/SimplePartition Logical Microlab Creation. Changing the name of the Quicksort group inside the Quicksort subject to Simple Partition

--- branches/DeployWags/client/src/webEditor/admin/builders/LMQuickSortDisplay.java
+++ branches/DeployWags/client/src/webEditor/admin/builders/LMQuickSortDisplay.java
@@ -21,7 +21,7 @@ public class LMQuickSortDisplay extends BasicDisplay {
 		canvas.nodeHandler.setUnique(false);
 		canvas.nodeHandler.setNumbers(true);
 		// As these will be numbers, we need to allow a greater range of digits
-		txtAddNode.setMaxLength(2);
+		txtAddNode.setMaxLength(3);
 		
 		txtInstructions.setText("Create a SimplePartition problem by adding" +
 		" the nodes to be sorted.  Nodes are evaluated left to right, not" +
