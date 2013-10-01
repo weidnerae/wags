5fd231fa4e5915078db0f9758569bcd19ac77fb1
cookdr22@gmail.com <cookdr22@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2013-10-01 04:43:17 +0000

    Button for testing Magnet Problems is now done except it shows up in a messed up tab.
    If I use the FlowPanel then every tab except the Problem Demo tab is bumped by 4-5 pixels.  If I use the HorizontalPanel then only the Problem Demo tab is bumped up. All of this has something to do with the 2.0 panels being created to work only in standards mode. 
    
    This link has some info: http://www.gwtproject.org/doc/latest/DevGuideUiPanels.html
    
    I could probably find a happy medium with the right concotion of panels but I need to take a break from this and move onto something else for a bit.

--- client/src/webEditor/admin/AdminPage.java
+++ client/src/webEditor/admin/AdminPage.java
@@ -3,9 +3,14 @@ package webEditor.admin;
 import webEditor.Proxy;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -49,7 +54,38 @@ public class AdminPage extends Composite {
 	}
 	
 	public void addWidgetInNewTab(Widget w, String tabTitle){
-		tabPanel.add(w, tabTitle);
-		w.setSize("100%", "100%");
+		// This is making me upset. If I use the FlowPanel then every tab except the Problem Demo tab is
+		// bumped by 4-5 pixels.  If I use the HorizontalPanel then only the Problem Demo tab is bumped up.
+		// All of this has something to do with the 2.0 panels being created to work only in standards mode.
+		// this link has some info: http://www.gwtproject.org/doc/latest/DevGuideUiPanels.html
+		// I could probably find a happy medium with the right concotion of panels but I need to take a break
+		// from this and move onto something else for a bit.
+		int currentTabIndex = tabPanel.getSelectedIndex();
+		// With this only the new tab with be messed up
+		HorizontalPanel tabWidget = new HorizontalPanel();
+		// With this every tab except the new tab will be messed up.
+		//FlowPanel tabWidget = new FlowPanel();
+		tabWidget.add(new InlineLabel(tabTitle+" "));
+		Button tabCloseButton = new Button("[X]", new CloseTabClickHandler(w, currentTabIndex, tabPanel));
+		tabWidget.add(tabCloseButton);
+		tabPanel.add(w, tabWidget);
+		tabPanel.selectTab(w);
 	}
+	
+	public class CloseTabClickHandler implements ClickHandler{
+		TabLayoutPanel tabPanel;
+		Widget widgetToClose;
+		int indexToOpen;
+		public CloseTabClickHandler(Widget widgetToClose,int indexToOpen, TabLayoutPanel tabPanel){
+			this.widgetToClose = widgetToClose;
+			this.indexToOpen = indexToOpen;
+			this.tabPanel = tabPanel;
+		}
+		@Override
+		public void onClick(ClickEvent event) {
+			tabPanel.selectTab(indexToOpen);
+			tabPanel.remove(widgetToClose);			
+		}
+	}
+	
 }
