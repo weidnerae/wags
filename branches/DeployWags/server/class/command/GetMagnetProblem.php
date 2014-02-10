e1fee8cf04b71ab278c8382cfe93b7aac0244698
cookdr22@gmail.com <cookdr22@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2014-02-10 17:18:27 +0000

    More comments about Prolog and State.

--- branches/DeployWags/server/class/command/GetMagnetProblem.php
+++ branches/DeployWags/server/class/command/GetMagnetProblem.php
@@ -18,12 +18,23 @@ class getMagnetProblem extends Command
         }
 
         $objArray = $magProb->toArray();
-        $id = $magProb->getId(); // necessary for getting state
+        $id = $magProb->getId(); // necessaryi for getting state
+        
+        // This disables state for Admins - We may want to change this.
+        // The reason was we didn't want the answer accidentaly being displayed
+        // Hopefully the instructor is able to quickly solve the problem.
         if(!Auth::getCurrentUser()->isAdmin()){
+            // Retrieves state from DB for this user and problem if one exists
             $state = MagnetProblemState::getEntryByProblemId($id);
         } else {
             $state = false;
         }
+        
+        // If there is a saved state for this user then we fetch any created
+        // magnets that got saved for this user and problem and add them
+        // onto the end of the list of statements.
+        // The created magnets will already have the right delimiter to 
+        // identify them as being created magnets by the client.
         if($state){
             $objArray['state'] = $state->getState();
             $createdMagnets = CreatedMagnet::getCreatedMagnetsByProblem($id);
--- branches/DeployWags/server/class/command/MagnetReview.php
+++ branches/DeployWags/server/class/command/MagnetReview.php
@@ -80,22 +80,27 @@ class MagnetReview extends Command
             if($lang == "java"){
                 exec("/usr/bin/php class/command/RunCodeNew.php $dir $driverName blank blank Java $nonce 2>&1", $stdout); 
             }else if($lang == "pl"){
+                // We have to do more if it's prolog because not all the
+                // Test/Helper classes are Java, we have a Prolog Solution in 
+                // there as well. We check for the ".pl" extension on the ClassName
+                // that we left in there in AddMagnetExercise.
                 $givenFilesArr = explode(" ",$givenFiles);
                 foreach($givenFilesArr as $filePath){
                     if(strpos($filePath, ".pl") !== false){
                         $solutionPath = $filePath;
                     }
                 }
-                $file = '/tmp/check.txt';
-                $file = fopen($file, "w");
-                fputs($file, "RunCodeNew gets: $solutionPath $driverName $studentPath");
-                fclose($file);
                 
                 // RunCodeNew adds the directory and file extension for prolog
                 // so we have to pull out the filename
                 $studentPath = pathinfo($studentPath)['filename'];
                 $solutionPath = pathinfo($solutionPath)['filename'];
 
+                // driverName is the uploaded Java Test file that will run
+                //     and compare the student and solution prolog files.
+                // solutionPath is the path of the uploaded Prolog solution.
+                // studentPath is the path of the file generated from the
+                //     student's magnets.
                 exec("/usr/bin/php class/command/RunCodeNew.php $dir $driverName $solutionPath $studentPath Prolog $nonce 2>&1",$stdout);
             }else{
                 JSON::warn("Error determining Language");
