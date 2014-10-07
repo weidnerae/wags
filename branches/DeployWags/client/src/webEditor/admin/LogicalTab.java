6a40bad53882cfa70545fb5100e4c469362ecee1
scottshuffler@gmail.com <scottshuffler@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2014-10-07 17:20:21 +0000

    Magnet Management face lift

--- branches/DeployWags/client/src/webEditor/admin/LogicalTab.java
+++ branches/DeployWags/client/src/webEditor/admin/LogicalTab.java
@@ -11,13 +11,13 @@ import webEditor.ProxyFramework.GetLMSubjectsCommand;
 import webEditor.ProxyFramework.SetLMExercisesCommand;
 import webEditor.WEStatus;
 
+import com.github.gwtbootstrap.client.ui.CheckBox;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
